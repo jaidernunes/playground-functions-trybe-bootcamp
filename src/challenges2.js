fd3faf415822f8a1070343491eaa1ac15d39eef2
jaidernunes <jaidernf@gmail.com>
2022-08-01 12:49:25 -0300

    bonus 12 :ok: - 11 and 13 to-do

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,26 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber() {}
+// seu código aqui
+//   let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+
+// if (arr.length !== 10) {
+//   console.log('Array com tamanho incorreto.')
+// } else if () {
+
+// }????????????????????????????????????????
+// }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
-}
+  if (lineA > lineB + lineC || lineB > lineA + lineC || lineC > lineA + lineB) {
+    return (false);
+  }
+  if (lineA < Math.abs(lineB - lineC) || lineB < Math.abs(lineA - lineC) || lineC < Math.abs(lineA - lineB)) {
+    return (false);
+  }
+    return (true);
+  }
 
 // Desafio 13
 function hydrate() {
