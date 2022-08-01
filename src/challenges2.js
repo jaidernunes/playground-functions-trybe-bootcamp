566ec420529d0d882933fed8ff51de5508158611
jaidernunes <jaidernf@gmail.com>
2022-08-01 16:35:29 -0300

    req, 11 :ok: - 13: to-do

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,14 +1,24 @@
 // Desafio 11
-function generatePhoneNumber() {}
-// seu código aqui
-//   let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+function generatePhoneNumber(arr) {
+  //seu código aqui
+  if (arr.length !== 11) {
+    return ('Array com tamanho incorreto.');
+  }
+  let repeats = 0;
+  let phoneNumber = '';
+  for (let index of arr) {
+    for (i = 0; i < arr.length; i += 1)
+      if (index === arr[i]) {
+        repeats += 1;
+      }
+    if (index > 9 || index < 0 || repeats >= 3) {
+      return ('não é possível gerar um número de telefone com esses valores');
+    } else { repeats = 0;}
+  }
+  phoneNumber = '(' + arr[0] + arr[1] + ') ' + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + '-' + arr[7] + arr[8] + arr[9] + arr[10];
+  return (phoneNumber)
+}
 
-// if (arr.length !== 10) {
-//   console.log('Array com tamanho incorreto.')
-// } else if () {
-
-// }????????????????????????????????????????
-// }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
